16fe2dbacbc0299afd2851f9910312fdec7cc516
Parth-Kulkarni26 <48427057+Parth-Kulkarni26@users.noreply.github.com>
2021-04-04 17:57:23 +0530

    Add project files.

---
+++ WebApiEF/Models/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApiEF.Models
+{
+    public enum Suppliers
+    {
+        S001, S002, S003, S004
+    }
+    public class Product
+    {
+        
+            [Key]
+            public int ProductId { get; set; }
+            [Required]
+            [MaxLength(20, ErrorMessage = "Max Length is 20")]
+            [MinLength(6, ErrorMessage = "Min Length is 6")]
+            public string ProductName { get; set; }
+
+            [Required]
+            [Range(1, 100, ErrorMessage = "Range between 0 and 100")]
+            public int QntyInStock { get; set; }
+            [Required]
+            public string Description { get; set; }
+
+            public Suppliers Suppiler { get; set; }
+            [DataType(DataType.Date)]
+            [Validation]
+            public DateTime DateOfPurchase { get; set; }
+        }
+    }
